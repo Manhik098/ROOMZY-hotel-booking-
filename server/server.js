92e3aa87cf041ad6190c7bd71d3dd806813f7530
Manhik098 <manhikkalyan@gmail.com>
2025-07-15 03:50:58 +0530

    temp commit

--- server/server.js
+++ server/server.js
@@ -1,54 +1,54 @@
 import express from "express"
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from 'dotenv';
+dotenv.config();
 import cors from "cors"
-import cookieParser from 'cookie-parser'
-import connectDB from "./configs/db.js"
-import connectCloudinary from "./configs/cloudinary.js"
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebHooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import cookieParser from 'cookie-parser';
 
-import { clerkMiddleware, ClerkExpressRequireAuth } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebHooks.js"
 
-import userRouter from "./routes/userRoutes.js"
-import hotelRouter from "./routes/hotelRoutes.js"
-import roomRouter from "./routes/roomRoutes.js"
-import bookingRouter from "./routes/bookingRoutes.js"
 
-const app = express()
+
 
 connectDB()
 connectCloudinary()
 
-const PORT = process.env.PORT || 3000
+const PORT= process.env.PORT || 3000;
 
+const app= express();
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'https://roomzy098.vercel.app',
-    'https://roomzy-hotel-booking-frontend-manhiks-projects.vercel.app'
-  ],
+  'http://localhost:5173',
+  'https://roomzy098.vercel.app',
+  'https://roomzy-hotel-booking-frontend-manhiks-projects.vercel.app'  // full deploy URL from Vercel
+],
+
   credentials: true
-}))
+}));       // Allow backend to connect with any frontend. cross-origin resource sharing
 
-app.use(cookieParser())
+app.use(cookieParser());
+//Middleware
 app.use(express.json())
-app.use(clerkMiddleware())  // Optional
+app.use(clerkMiddleware())
 
-// Webhook route (Clerk needs this)
-app.use("/api/clerk", clerkWebhooks)
+// Api to listen to clerk webhooks
+app.use("/api/clerk",clerkWebhooks)
 
-// Public routes (No auth required)
-app.use('/api/hotels', hotelRouter)
-app.use('/api/rooms', roomRouter)
 
-// Private routes (Require Clerk auth)
-app.use('/api/user', ClerkExpressRequireAuth(), userRouter)
-app.use('/api/bookings', ClerkExpressRequireAuth(), bookingRouter)
+app.get('/',(req,res)=>{
+    res.send("working hurr ")
+});
+app.use('/api/user',userRouter)
+app.use('/api/hotels',hotelRouter)
+app.use('/api/rooms',roomRouter)
+app.use('/api/bookings',bookingRouter)
 
-app.get('/', (req, res) => {
-  res.send("working hurr ")
-})
-
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`)
-})
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+});
