e357ad85220863d24395cfa31095441001a4a85c
Manhik098 <manhikkalyan@gmail.com>
2025-07-15 20:07:33 +0530

    temp 13

--- server/server.js
+++ server/server.js
@@ -21,8 +21,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'https://roomzy098.vercel.app',
+    'http://localhost:3000'
   ],
   credentials: true
 }));
